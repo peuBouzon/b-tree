af564be7b8329cd2d9dbad90504d7ceae72ff7b7
pedro.bouzon <peubouzon@gmail.com>
2025-02-27 16:51:58 -0300

    add arguments for input and output files

--- trab2.py
+++ trab2.py
@@ -1,16 +1,22 @@
 from btree import BTree
+import argparse
 
-with open('./casos_teste/caso_teste_1.txt', 'r') as f:
-    max_entries_per_node = int(f.readline().strip())
-    n_operations = int(f.readline().strip())
-    btree = BTree(max_entries_per_node)
-    for line in f:
-        args = line.split(' ')
-        command = args[0].strip()
-        arg1 = int(args[1].strip().replace(',', ''))
-        if command == 'I':
-            btree.put(arg1, int(args[2].strip().replace(',', '')))
-        elif command == 'R':
-            btree.delete(arg1)
-        elif command == 'B':
-            print('O REGISTRO ESTA NA ARVORE!' if btree.get(arg1) is not None else 'O REGISTRO NAO ESTA NA ARVORE!')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    parser.add_argument('output')
+    args = parser.parse_args()
+    with open(args.input, 'r') as f:
+        max_entries_per_node = int(f.readline().strip())
+        n_operations = int(f.readline().strip())
+        btree = BTree(max_entries_per_node)
+        for line in f:
+            args = line.split(' ')
+            command = args[0].strip()
+            arg1 = int(args[1].strip().replace(',', ''))
+            if command == 'I':
+                btree.put(arg1, int(args[2].strip().replace(',', '')))
+            elif command == 'R':
+                btree.delete(arg1)
+            elif command == 'B':
+                print('O REGISTRO ESTA NA ARVORE!' if btree.get(arg1) is not None else 'O REGISTRO NAO ESTA NA ARVORE!')
