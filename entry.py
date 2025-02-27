a4678b9a9e4bcb88fccfbd8aaa2283d811534bd3
pedro.bouzon <peubouzon@gmail.com>
2025-02-27 15:58:23 -0300

    add __repr__

--- entry.py
+++ entry.py
@@ -1,7 +1,8 @@
-from node import Node
-
 class Entry:
-    def __init__(self, key, value, next_node : Node) -> None:
+    def __init__(self, key, value, next_node) -> None:
         self.key = key
         self.value = value
         self.next_node = next_node
+
+    def __repr__(self) -> str:
+        return f'{self.key}: {self.value}'
