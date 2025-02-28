e04efa7c99c0ec5cde0b21b8e1d7721bef3c2e38
pedro.bouzon <peubouzon@gmail.com>
2025-02-28 14:43:23 -0300

    fix output

--- trab2.py
+++ trab2.py
@@ -10,13 +10,19 @@ if __name__ == '__main__':
         max_entries_per_node = int(f.readline().strip())
         n_operations = int(f.readline().strip())
         btree = BTree(max_entries_per_node)
-        for line in f:
-            args = line.split(' ')
-            command = args[0].strip()
-            arg1 = int(args[1].strip().replace(',', ''))
-            if command == 'I':
-                btree.put(arg1, int(args[2].strip().replace(',', '')))
-            elif command == 'R':
-                btree.delete(arg1)
-            elif command == 'B':
-                print('O REGISTRO ESTA NA ARVORE!' if btree.get(arg1) is not None else 'O REGISTRO NAO ESTA NA ARVORE!')
+        processed = 0
+        with open(args.output, 'w') as output_file:
+            for line in f:
+                args = line.split(' ')
+                command = args[0].strip()
+                arg1 = int(args[1].strip().replace(',', ''))
+                if command == 'I':
+                    btree.put(arg1, int(args[2].strip().replace(',', '')))
+                elif command == 'R':
+                    btree.delete(arg1)
+                elif command == 'B':
+                    output_file.write('O REGISTRO ESTA NA ARVORE!' if btree.get(arg1) is not None else 'O REGISTRO NAO ESTA NA ARVORE!')
+                    if processed < n_operations - 1:
+                        output_file.write('\n')
+                processed += 1
+        
