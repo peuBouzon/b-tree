bcc08c49da9f5d8fd4fb0338af419dd787a6f7d8
pedro.bouzon <peubouzon@gmail.com>
2025-03-01 17:44:17 -0300

    fix insertion

--- btree.py
+++ btree.py
@@ -5,9 +5,9 @@ import math
 
 
 class BTree:
-    def __init__(self, max_entries_per_node = 1000) -> None:
-        self.max_entries_per_node = max_entries_per_node
-        self.root = Node(0, max_entries_per_node)
+    def __init__(self, degree = 1000) -> None:
+        self.degree = degree
+        self.root = Node(0, degree)
         self.height = 0
         self.n_entries = 0
 
@@ -27,62 +27,82 @@ class BTree:
         if height == 0:
             return
 
-        return self._search(node.children[i], key, height - 1)
-        
+        return self._search(node.children[i + 1], key, height - 1)
+
     def put(self, key, value):
-        if len(self.root) >= self.max_entries_per_node - 1:
+        if len(self.root) >= self.degree - 1:
             old_root = self.root
-            new_root = Node(0, self.max_entries_per_node)
+            new_root = Node(0, self.degree)
             new_root.children[0] = old_root
             self.root = new_root
-            self._split_child(new_root, 1, old_root, self.height)
-            print(new_root)
-            self._insert(new_root, key, value, self.height)
+            self.height += 1
+            self._split_child(self.root, 0, new_root.children[0])
+            self._insert_non_full(self.root, key, value, self.height) 
         else:
-            self._insert(self.root, key, value, self.height)
+            self._insert_non_full(self.root, key, value, self.height)
 
-    def _insert(self, node : Node, key, value, height : int):
-        i = len(node) - 1
-        if height == 0:
-            while i >= 1 and key < node.keys[i]:
-                node.keys[i + 1] = node.keys[i]
-                i -= 1
-            node.keys[i + 1] = key
-            node.values[i + 1] = value
-            node.n_elements += 1
+    def _insert_non_full(self, node : Node, key, value, height : int):
+        if height == 0: # is leaf
+            self._insert_non_full_leaf(node, key, value)
         else:
-            while i >= 1 and key < node.keys[i]:
+            # find the index of the smallest key greater then the new key
+            # this will be used to insert into the correct child
+
+            i = len(node) - 1
+            while i >= 0 and key < node.keys[i]:
                 i -= 1
             i = i + 1
-            child = node.children[i]
-            if len(child) == self.max_entries_per_node - 1:
-                self._split_child(node, i, child)
-                if key > node.keys[i]:
+
+            # split child if full
+            if len(node.children[i]) >= self.degree - 1:
+                self._split_child(node, i, node.children[i])
+                # split child adds a key to the parent node,
+                # we need to check if this key is the smallest key greater than the key to be inserted
+                if key > node.keys[i + 1]:
                     i += 1
-            self._insert(child, key, value, height - 1)
 
-    def _split_child(self, node : Node, index : int, child : Node, height_child : int):
-        index_median = math.ceil(self.max_entries_per_node / 2) - 1
-        new_node = Node(self.max_entries_per_node // 2, self.max_entries_per_node)
-        for j in range(1, index_median):
-            new_node.keys[j] = child.keys[j + index_median]
+            self._insert_non_full(node.children[i], key, value, height - 1)
 
-        if height_child != 0:
-            for j in range(0, index_median):
-                new_node.children[j] = child.children[j + index_median]
+    # FIXME: leafs don't need to copy children
+    def _split_child(self, parent : Node, child_index : int, child : Node):
+        n_keys_child = len(child)
+        half = n_keys_child // 2
+        new_sibling = Node(half - 1 if n_keys_child % 2 == 0 else half, self.degree)
+        child.n_entries = half
 
-        child.n_elements = self.max_entries_per_node - index_median
+        new_sibling.children[0] = child.children[half]
+        for j in range(0, len(new_sibling)):
+            index_child = j + half + 1
+            new_sibling.keys[j] = child.keys[index_child]
+            new_sibling.values[j] = child.values[index_child]
+            new_sibling.children[j + 1] = child.children[index_child]
 
-        for j in range(len(node) + 1, index, -1):
-            node.children[j] = node.children[j - 1]
+        for i in range(len(parent) - 1, child_index - 2, -1):
+            if i >= 0:
+                parent.keys[i + 1] = parent.keys[i]
+                parent.values[i + 1] = parent.values[i]
+            parent.children[i + 2] = parent.children[i + 1]
 
-        for j in range(len(node), index, -1):
-            node.keys[j] = node.keys[j - 1]
+        parent.keys[child_index] = child.keys[half]
+        parent.values[child_index] = child.values[half]
+        parent.children[child_index] = new_sibling
+        parent.n_entries += 1
 
-        node.children[index] = new_node
-        node.keys[index] = new_node.keys[0]
-        node.n_elements += 1
+    def _insert_non_full_leaf(self, node : Node, key, value):
+        i = len(node) - 1
+        while i >= 0 and key < node.keys[i]:
+            node.keys[i + 1] = node.keys[i]
+            node.values[i + 1] = node.values[i]
+            i -= 1
 
+        i += 1
+        if node.keys[i] == key:
+            node.values[i] = value
+            return
+
+        node.keys[i] = key
+        node.values[i] = value
+        node.n_entries += 1
 
     def delete(self, key):
         pass
